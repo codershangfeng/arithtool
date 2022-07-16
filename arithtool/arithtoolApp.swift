82baa3111e9d18998ff8b451c5553b0f4977c68f
codershangfeng <codershangfeng@gmail.com>
2022-07-16 14:27:44 +0800

    Initial Commit

---
+++ arithtool/arithtoolApp.swift
@@ -0,0 +1,17 @@
+//
+//  arithtoolApp.swift
+//  arithtool
+//
+//  Created by sam on 2022/7/16.
+//
+
+import SwiftUI
+
+@main
+struct arithtoolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
