3a7afadd641c49cfe61de7be7a73e8c5a63a60f1
codershangfeng <codershangfeng@gmail.com>
2022-07-18 21:18:22 +0800

    generate multiple division quizzes along with addition/substraction ones.

--- arithtool/ContentView.swift
+++ arithtool/ContentView.swift
@@ -8,45 +8,71 @@
 import SwiftUI
 
 struct ContentView: View {
-    var genQuizzesFunc: (() -> [quiz]) =  { return [] }
-    @State private var quizzes: [quiz] = []
+    var genQuizzesFunc: ((Mode) -> [expression]) =  { _ in return [] }
+    @State private var additionAndSubstractionExps: [expression] = []
+    @State private var divisionExps: [expression] = []
     @State private var showResult: Bool = false
 
     var body: some View {
         VStack {
             Spacer()
-            HStack(alignment: .center) {
-                if quizzes.count == 0 {
-                    EquationView(leftOperandText: "?", rightOperandText: "?", operatorText: "+/-", resultText: "?")
-                } else {
-                    VStack(alignment: .center) {
-                        ForEach(quizzes) { quiz in
-                            if showResult {
-                                EquationView(leftOperandText: "\(quiz.leftOperand)", rightOperandText: "\(quiz.rightOperand)", operatorText: quiz.op , resultText: "\(quiz.sum())")
-                                    .padding()
-                            } else {
-                                EquationView(leftOperandText: "\(quiz.leftOperand)", rightOperandText: "\(quiz.rightOperand)", operatorText: quiz.op , resultText: "?")
-                                    .padding()
+            HStack() {
+                Spacer()
+                VStack() {
+                    if additionAndSubstractionExps.count == 0 {
+                        EquationView(leftOperandText: "?", rightOperandText: "?", operatorText: "+ or -", resultText: "?")
+                    } else {
+                        VStack(alignment: .center) {
+                            ForEach(additionAndSubstractionExps) { exp in
+                                if showResult {
+                                    EquationView(leftOperandText: "\(exp.leftOperand)", rightOperandText: "\(exp.rightOperand)", operatorText: exp.op , resultText: "\(exp.total())")
+                                        .padding()
+                                } else {
+                                    EquationView(leftOperandText: "\(exp.leftOperand)", rightOperandText: "\(exp.rightOperand)", operatorText: exp.op , resultText: "?")
+                                        .padding()
+                                }
                             }
                         }
                     }
                 }
+                Spacer()
+                Divider()
+                Spacer()
+                VStack() {
+                    if divisionExps.count == 0 {
+                        EquationView(leftOperandText: "?", rightOperandText: "?", operatorText: "÷", resultText: "?")
+                    } else {
+                        VStack(alignment: .center) {
+                            ForEach(divisionExps) { exp in
+                                if showResult {
+                                    EquationView(leftOperandText: "\(exp.leftOperand)", rightOperandText: "\(exp.rightOperand)", operatorText: "÷" , resultText: "\(exp.quotient()) ······ \(exp.remainder())")
+                                        .padding()
+                                } else {
+                                    EquationView(leftOperandText: "\(exp.leftOperand)", rightOperandText: "\(exp.rightOperand)", operatorText: "÷", resultText: "? ······ ?")
+                                        .padding()
+                                }
+                            }
+                        }
+                    }
+                }
+                Spacer()
             }
             Spacer()
             Divider()
             HStack {
                 Spacer()
-                Button("Generate") {
-                    quizzes = self.genQuizzesFunc()
+                Button(" 生成 ") {
+                    additionAndSubstractionExps = self.genQuizzesFunc(.additionAndSubstraction)
+                    divisionExps = self.genQuizzesFunc(.division)
                     showResult = false
                 }.background(Color.blue)
                     .foregroundColor(Color.white)
                     .cornerRadius(5)
                     .padding()
                 Spacer()
-                Button("Reveal") {
+                Button(" 答案 ") {
                     showResult = true
-                }.disabled(quizzes.count == 0 || showResult == true)
+                }.disabled(additionAndSubstractionExps.count == 0 || showResult == true)
                     .background(Color.red)
                     .foregroundColor(Color.white)
                     .cornerRadius(5)
@@ -65,7 +91,7 @@ struct EquationView: View {
     var resultText: String = ""
 
     var body: some View {
-        HStack {
+        HStack(alignment: .center) {
             Text(leftOperandText)
             Text(operatorText)
             Text(rightOperandText)
--- arithtool/arithtoolApp.swift
+++ arithtool/arithtoolApp.swift
@@ -11,18 +11,18 @@ import SwiftUI
 struct arithtoolApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView(genQuizzesFunc: genQuizzes)
+            ContentView(genQuizzesFunc: genExpressions)
         }
     }
 }
 
-struct quiz: Identifiable {
+struct expression: Identifiable {
     let id = UUID()
     var leftOperand: Int
     var op: String
     var rightOperand: Int
     
-    func sum() -> Int {
+    func total() -> Int {
         switch self.op {
         case "+":
             return self.leftOperand + self.rightOperand
@@ -33,31 +33,49 @@ struct quiz: Identifiable {
         }
     }
     
-    func equation() -> String {
-        return "\(self.leftOperand) \(self.op) \(self.rightOperand) = \(self.sum())"
+    func quotient() -> Int {
+        return self.leftOperand / self.rightOperand
     }
     
-    func question() -> String {
-        return "\(self.leftOperand) \(self.op) \(self.rightOperand) = ?"
+    func remainder() -> Int {
+        return self.leftOperand % self.rightOperand
     }
 }
 
-func genQuizzes() -> [quiz] {
-    var quizzes: [quiz] = []
-    
-    while quizzes.count <= 4 {
-        let left = Int.random(in: 1...999)
-        let right = Int.random(in: 1...999)
-        
-        let sum = left + right
-        let difference = left - right
-        if sum < 1000 {
-            quizzes.append(quiz(leftOperand: left, op: "+", rightOperand: right))
-        } else if difference > 0 && difference > 0 {
-            quizzes.append(quiz(leftOperand: left, op: "-", rightOperand: right))
+func genExpressions(mode: Mode) -> [expression] {
+    var exps: [expression] = []
+    let cnt = 4
+    switch mode {
+    case .additionAndSubstraction:
+        while exps.count <= cnt {
+            let left = Int.random(in: 100...999)
+            let right = Int.random(in: 100...999)
+
+            let sum = left + right
+            let difference = left - right
+            if sum < 1000 {
+                exps.append(expression(leftOperand: left, op: "+", rightOperand: right))
+            } else if difference > 0 && difference > 0 {
+                exps.append(expression(leftOperand: left, op: "-", rightOperand: right))
+            }
+        }
+    case .division:
+        while exps.count <= cnt {
+            let divisor = Int.random(in: 3...9)
+            var reminder = 10
+            while reminder >= divisor {
+                reminder = Int.random(in: 2...9)
+            }
+            let dividend = divisor * Int.random(in: 1...9) + reminder
+
+            exps.append(expression(leftOperand: dividend, op: "/", rightOperand: divisor))
         }
     }
 
-    return quizzes
+    return exps
 }
 
+enum Mode {
+    case additionAndSubstraction
+    case division
+}
